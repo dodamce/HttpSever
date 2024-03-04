1f6bccf15a36b4312bc7b8525cda257b5fab596a
dodamce <3030949391@qq.com>
2024-03-04 19:37:27 +0800

    完成Http请求响应类的构建

--- Protocol.hpp
+++ Protocol.hpp
@@ -4,9 +4,63 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include "./tool/Util.hpp"
+#include <string>
+#include <vector>
 
 // 协议读取分析工作(每个线程工作任务)
+// 请求报文信息
+struct HttpRequest
+{
+    std::string req_line;               // 请求行
+    std::vector<std::string> req_heads; // 请求报头
+    std::string blank;                  // 空行
+    std::string req_body;               // 请求正文
+};
+// 响应报文信息
+struct HttpResponse
+{
+    std::string res_line;               // 状态行
+    std::vector<std::string> res_heads; // 响应报头
+    std::string blank;                  // 空行
+    std::string res_body;               // 响应正文
+};
+// 对端业务逻辑类,读取请求，分析请求，构建响应。基本IO通信
+class EndPoint
+{
+private:
+    int sock;
+    HttpRequest request;
+    HttpResponse response;
 
+    void RecvRequestLine()
+    {
+        Util::readLine(sock, request.req_line);
+    }
+    void RecvRequestHander()
+    {
+        
+    }
+
+public:
+    EndPoint(int _sock)
+    {
+        sock = _sock;
+    }
+    ~EndPoint()
+    {
+        close(sock);
+    }
+    // 读取请求
+    void ReadRequest()
+    {
+    }
+    // 解析请求
+    void ParseRequest() {}
+    // 构建响应
+    void BuildRequest() {}
+    // 发送响应
+    void SendRequest() {}
+};
 // 线程工作入口
 class Entrance
 {
@@ -16,11 +70,12 @@ public:
     {
         int sock = *(int *)_sock;
         delete (int *)_sock;
-        std::cout << "Get a new link sock=" << sock << std::endl;
-        std::string line;
-        Util::readLine(sock, line);
-        std::cout << line << std::endl;
-        close(sock);
+        EndPoint *endpoint = new EndPoint(sock);
+        endpoint->ReadRequest();
+        endpoint->ParseRequest();
+        endpoint->BuildRequest();
+        endpoint->SendRequest();
+        delete endpoint;
         return nullptr;
     }
 };
