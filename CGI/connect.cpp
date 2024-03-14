a7923708c65fdc7456f6aad3ed99b3df3bba3dfe
dodamce <3030949391@qq.com>
2024-03-14 20:48:17 +0800

    测试链接数据库

--- CGI/connect.cpp
+++ CGI/connect.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include "./include/mysql.h"
+#include <string>
 using namespace std;
 int main(int argc, char const *argv[])
-{ 
-    cout << mysql_get_client_info() << endl;
+{
+    MYSQL *connect = mysql_init(nullptr);
+    mysql_set_character_set(connect, "utf8"); // 设置编码格式，防止乱码
+    // mysql connect+ ip + user name + password + database name + port +null+0
+    if (mysql_real_connect(connect, "127.0.0.1", "dodamce", "000000", "HttpSever", 3306, nullptr, 0) == nullptr)
+    {
+        cerr << "DEBUG error connecting!:" << mysql_error(connect) << endl;
+        return 1;
+    }
+    cerr << "DEBUG succeed connecting" << endl;
+    std::string sql = "insert into user (name,passward) values (\'测试\',\'000000\')";
+    cout << "DEBUG sql:" << sql << endl;
+    int ret = mysql_query(connect, sql.c_str());
+    if (ret != 0)
+    {
+        cerr << "DEBUG error querying!:" << mysql_error(connect) << endl;
+        return 1;
+    }
+    mysql_close(connect);
     return 0;
 }
